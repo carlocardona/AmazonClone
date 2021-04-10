4d33b6423b8dd67ea451c4614698b60661fd0d31
Carlo <carlo.dev2@gmail.com>
2021-04-10 23:43:08 +1000

    leaving at adding search icon

--- src/App.js
+++ src/App.js
@@ -1,47 +1,27 @@
-import React, { useReducer, useRef } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from './components/Header';
 
-const TodoList = () => {
-
-  const inputRef = useRef();
-
-  const [items, dispatch] = useReducer((items, action) => {
-    console.log(inputRef.current.value);
-    if (action === "add") {
-      const value = inputRef.current.value;
-      return [...items, value];
-    }
-
-    if (action.type === "delete"){
-      return items.filter((item,index) => index !== action.index)
-    }
-    return items;
-  }, []);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-  }
-
+function App() {
   return (
-    <div className="container text-center">
-      <h1>Create a Todo List!</h1>
-      <form className="form-group mt-5" onSubmit={handleSubmit}>
-        <input className="form-control" placeholder="Start typing what you need to do..." ref={inputRef} />
-        <button className="btn btn-success mt-3 mb-5" type="submit" onClick={() => dispatch("add")}>
-          Add to List
-        </button>
-      </form>
-      <h4>My Todo List:</h4>
-      <ul className="list-group">
-        {items.map((item, index) => (
-          <li className="list-group-item" key={index}>
-            {item}{" "}
-            <button onClick={() => dispatch({type: "delete", index: index})}>Delete</button>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <>
+    <Router>
+      <div className="app">
+        <Switch>
+          <Route path='/checkout'>
+            <h1>Checkout</h1>
+          </Route>
+          <Route path='/login'>
+            <h1>Login Page</h1>
+          </Route>
+          <Route path='/'>
+            <Header />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+   </>
   );
-};
+}
 
-export default TodoList;
+export default App;
